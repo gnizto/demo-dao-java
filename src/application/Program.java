5973dc41b17ae476dcd42a2605c92e91bf7eb11c
gnizto <gnizto@outlook.com>
2024-12-19 17:24:24 -0300

    Seller Entity Class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,19 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		Department d = new Department(1, "Books");
 		System.out.println(d);
+		
+		Seller s = new Seller(21, "Bob", "bob@gmail.com", LocalDate.now(), 3000.0, d);
+		
+		System.out.println(s);
 	}
 
 }
